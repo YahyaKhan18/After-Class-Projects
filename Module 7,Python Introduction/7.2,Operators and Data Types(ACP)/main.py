8408e34bfed12044df50d2f07519655750e1b5bb
YahyaKhan18 <yahyakhan1813@gmail.com>
2025-02-07 14:51:42 +0500

    7.2

---
+++ Module 7,Python Introduction/7.2,Operators and Data Types(ACP)/main.py
@@ -0,0 +1,12 @@
+x = input("Enter value of x:")
+y = input("Enter value of y:")
+z = input("Enter value of z:")
+
+temp = x
+x = y
+y = z
+z = temp
+
+print("Value of x after swapping:",x)
+print("Value of y after swapping:",y)
+print("Value of z after swapping:",z)
