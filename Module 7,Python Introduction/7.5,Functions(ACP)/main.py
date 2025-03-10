c3459f24d5ceb9a73f42b3a92bb666394bb02617
YahyaKhan18 <yahyakhan1813@gmail.com>
2025-03-10 21:35:54 +0500

    7.5

---
+++ Module 7,Python Introduction/7.5,Functions(ACP)/main.py
@@ -0,0 +1,11 @@
+sum = 0 + 1
+next = sum + 1
+next2 = next + sum
+next3 = next2 + next
+next4 = next3 + next2
+next5 = next4 + next3
+next6 = next5 + next4
+next7 = next6 + next5
+next8 = next7 + next6
+
+print(sum,next,next2,next3,next4,next5,next6,next7,next8)
