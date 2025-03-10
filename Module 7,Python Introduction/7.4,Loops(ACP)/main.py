19eccda25a16a814aa6f24a51c2f971fa625d9c0
YahyaKhan18 <yahyakhan1813@gmail.com>
2025-03-10 21:16:45 +0500

    7.4

--- Module 7,Python Introduction/7.4,Loops(ACP)/main.py
+++ Module 7,Python Introduction/7.4,Loops(ACP)/main.py
@@ -1,1 +1,17 @@
-input("Enter a number")
+x = int(input("Enter first digit : "))
+y = int(input("Enter second digit : "))
+z = int(input("Enter third digit : "))
+
+num = x*100 + y*10 + z
+
+dig1 = x**3
+dig2 = y**3
+dig3 = z**3
+
+sum = dig1 + dig2 + dig3
+
+if sum == num:
+    print("This is an armstrong number")
+
+else:
+    print("This is not an armstrong number")
